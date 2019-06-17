76519756029b39f2f76e97c696b1b05817356244
Fedor <novikovfedor@gmail.com>
2019-06-17 14:08:44 +0300

    add namespace

--- tests.h
+++ tests.h
@@ -9,123 +9,144 @@
 
 using namespace std;
 
-template <typename T>
-inline void packed_args_to_string(T& arg) {
-	try
-	{
-		cout << arg << endl;
-	}
-	catch (const std::exception& e)
-	{
-		cout << "non string arg: " << e.what() << endl;
-	}
-}
-
-template <typename T, typename ...MoreT>
-inline void packed_args_to_string(T& arg, MoreT& ...args) {
-	cout << arg << endl;
-	packed_args_to_string(args...);
-}
-
-
-template <typename T>
-inline bool check_test(T expected, T result) {
-	if (result == expected) {
-		cout << "\nsuccess: ";
-		return true;
-	}
-	cout << "\nfail: ";
-	cout << "result\n" << result << "\nexpected \n" << expected << "\n";
-	return false;
-}
-
-template <typename ExpectedT, typename MethodT, typename ...InT>
-inline void run_test(ExpectedT& expected, MethodT& method, InT& ...in_args) {
-	//cout << "test " << endl;
-
-	clock_t start = clock();
-	if (!check_test(expected, method(in_args...)))
-	{
-		packed_args_to_string(in_args...);
-	}
-	cout << "duration " << (clock() - start) / (double)CLOCKS_PER_SEC << " sec" << endl;
-}
-
-template <typename ExpectedT, typename MethodT, typename ...InT>
-inline void run_test(ExpectedT&& expected, MethodT& method, InT&& ...in_args) {
-	//cout << "test " << endl;
-
-	clock_t start = clock();
-	if(!check_test(expected, method(in_args...)))
-	{
-		packed_args_to_string(in_args...);
-	}
-	cout << "duration " << (clock() - start) / (double)CLOCKS_PER_SEC << " sec" << endl;
-}
-
-inline void trim_end(string& str)
-{
-	int end = str.find(" \n");
-	while (end != string::npos)
-	{
-		str.erase(end, 1);
-		end = str.find(" \n");
-	}
-}
-
-
-inline void clearify(string& expected, string& result)
-{
-	trim_end(expected);
-	trim_end(result);
-
-	if (*expected.rbegin() == '\n') expected.erase(expected.size() - 1, 1);
-	if (*result.rbegin() == '\n') result.erase(result.size() - 1, 1);
-}
-
-template <typename MethodT>
-inline void run_test_from_text(MethodT& method, string&& filename) {
-	ifstream file_stream(filename);
-	int test_num = 1;
-
-	try
-	{
-		while (!file_stream.eof())
+namespace tests {
+	template <typename T>
+	inline void packed_args_to_string(T& arg) {
+		try
 		{
-			cout << "test case " << test_num++;
-			std::ostringstream out_stream;
-
-			clock_t start = clock();
-
-			method(file_stream, out_stream);
-			string result = out_stream.str();
-			string expected;
-			string line;
-			do
-			{
-				getline(file_stream, line);
-			} while (line.empty() && !file_stream.eof());
-
-			do 
-			{
-				expected += line + " \n";
-				if (file_stream.eof())
-					break;
-				getline(file_stream, line);
-			} while (!line.empty());
-			
-			clearify(expected, result);
-
-			check_test(expected, result);
-
-			cout << "duration " << (clock() - start) / (double)CLOCKS_PER_SEC << " sec" << endl;
+			cout << arg << endl;
 		}
-
-		file_stream.close();
+		catch (const std::exception& e)
+		{
+			cout << "non string arg: " << e.what() << endl;
+		}
 	}
-	catch (...)
+
+	template <typename T, typename ...MoreT>
+	inline void packed_args_to_string(T& arg, MoreT& ...args) {
+		cout << arg << endl;
+		packed_args_to_string(args...);
+	}
+
+
+	template <typename T>
+	inline bool check_test(T expected, T result) {
+		if (result == expected) {
+			cout << "\nsuccess: ";
+			return true;
+		}
+		cout << "\nfail: ";
+		cout << "result\n" << result << "\nexpected \n" << expected << "\n";
+		return false;
+	}
+
+	template <typename ExpectedT, typename MethodT, typename ...InT>
+	inline void run_test(ExpectedT& expected, MethodT& method, InT& ...in_args) {
+		//cout << "test " << endl;
+
+		clock_t start = clock();
+		if (!check_test(expected, method(in_args...)))
+		{
+			packed_args_to_string(in_args...);
+		}
+		cout << "duration " << (clock() - start) / (double)CLOCKS_PER_SEC << " sec" << endl;
+	}
+
+	template <typename ExpectedT, typename MethodT, typename ...InT>
+	inline void run_test(ExpectedT&& expected, MethodT& method, InT&& ...in_args) {
+		//cout << "test " << endl;
+
+		clock_t start = clock();
+		if (!check_test(expected, method(in_args...)))
+		{
+			packed_args_to_string(in_args...);
+		}
+		cout << "duration " << (clock() - start) / (double)CLOCKS_PER_SEC << " sec" << endl;
+	}
+
+	inline void trim_end(string& str)
 	{
-		file_stream.close();
-		throw;
+		int end = str.find(" \n");
+		while (end != string::npos)
+		{
+			str.erase(end, 1);
+			end = str.find(" \n");
+		}
+		if (*str.rbegin() == '\n') str.erase(str.size() - 1, 1);
+		if (*str.rbegin() == ' ') str.erase(str.size() - 1, 1);
+	}
+
+
+	inline void clearify(string& expected, string& result)
+	{
+		trim_end(expected);
+		trim_end(result);
+	}
+
+	template <typename MethodT>
+	inline bool run_test_from_text(MethodT& method, string&& filename) {
+		ifstream file_stream(filename);
+		int test_num = 1;
+		bool res = false;
+
+		try
+		{
+			while (!file_stream.eof())
+			{
+				cout << "test case " << test_num++;
+				std::ostringstream out_stream;
+
+				clock_t start = clock();
+
+				method(file_stream, out_stream);
+				string result = out_stream.str();
+				string expected;
+				string line;
+				do
+				{
+					getline(file_stream, line);
+				} while (line.empty() && !file_stream.eof());
+
+				do
+				{
+					expected += line + " \n";
+					if (file_stream.eof())
+						break;
+					getline(file_stream, line);
+				} while (!line.empty());
+
+				clearify(expected, result);
+
+				res = check_test(expected, result);
+
+				cout << "duration " << (clock() - start) / (double)CLOCKS_PER_SEC << " sec" << endl << endl;
+			}
+
+			file_stream.close();
+			return res;
+		}
+		catch (...)
+		{
+			file_stream.close();
+			throw;
+		}
+	}
+
+	template <typename MethodT>
+	inline void run_from_text(MethodT& method, string&& filename, string&& outfilename) {
+		ifstream file_stream(filename);
+		ofstream out_stream(outfilename, std::ofstream::out | std::ofstream::trunc);
+		try
+		{
+			method(file_stream, out_stream);
+			file_stream.close();
+			out_stream.close();
+		}
+		catch (...)
+		{
+			file_stream.close();
+			out_stream.close();
+			throw;
+		}
 	}
 }
